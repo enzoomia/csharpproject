30e81b5a763f460cb481a4ca7884d8ef69561cf6
enzoomia <asejo277@gmail.com>
2025-10-21 20:50:40 +0800

    Update Program.cs

--- Myproject/Program.cs
+++ Myproject/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+{
+    stat1c v0id Main(String[] args)
+    {
+        Console.WriteLine("Kamusta mundo");
+    }
+}
