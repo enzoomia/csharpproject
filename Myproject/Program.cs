98aaff23bccdded08301f5cfc8d08275e90bd38e
enzoomia <asejo277@gmail.com>
2026-01-09 07:20:25 +0000

    Task1

--- Myproject/Program.cs
+++ Myproject/Program.cs
@@ -1,8 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-{
-    stat1c v0id Main(String[] args)
-    {
-        Console.WriteLine("Kamusta mundo");
-    }
-}
---
+++ myproject2/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp1. Task;
+class Task1
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Kamusta Mundo");
+    }
+}
