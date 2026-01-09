e3046b52538c3ed551bf9cb49b0fa8edac00f9dc
enzoomia <asejo277@gmail.com>
2026-01-09 08:07:03 +0000

    task4

--- myproject2/Program.cs
+++ myproject2/Program.cs
@@ -1,27 +1,67 @@
 ﻿namespace ConsoleApp1. Task;
-class Task3
+class task4
 {
-    static void Main(string[] args)
+    public static float AddTwoNums(float a,float b)
     {
-        int[] numbers = { 3, 7, 12, 19, 21, 25, 30 };
-        System.Console.WriteLine("Enter a number: ");
-        int numberToSearch = int.Parse(Console.ReadLine());
-        // System.Console.WriteLine(1+numberToSearch);
-        int index = 0;
-        bool found = false;
-        foreach (var number in numbers)
+        return a+b;
+    }
+    public static float SubTwoNums(float a,float b)
+    {   
+        return a-b;       
+    } 
+    public static float MulTwoNums(float a,float b)
+    {   
+        return a*b;       
+    } 
+    public static float DivTwoNums(float a,float b)
+    {   
+        return a/b;       
+    } 
+
+    public static void ask()
+    {
+        System.Console.WriteLine("1 - Addition");
+        System.Console.WriteLine("2 - Subtraction");
+        System.Console.WriteLine("3 - Multipliation");
+        System.Console.WriteLine("4 - Division");
+        int option = 0;
+        option= int.Parse(Console.ReadLine());
+        System.Console.Write("Enter Value 1: ");
+        float val1 =int.Parse(Console.ReadLine());
+        System.Console.Write("Enter Value 2: ");
+        float val2 =int.Parse(Console.ReadLine());
+        switch (option)
         {
-            if (number == numberToSearch)
-            {
-                System.Console.WriteLine( $"Number found at position {index}!" );
-                found = true;
-                break;
-            }
-            index++;
-        }
-        if (!found)
-        {
-            System.Console.WriteLine( "Number not found in the list." );
+            case 1: System.Console.WriteLine($"{val1} + {val2} = {AddTwoNums(val1,val2)}");
+            break;
+            case 2: System.Console.WriteLine($"{val1} - {val2} = {SubTwoNums(val1,val2)}");
+            break;
+            case 3: System.Console.WriteLine($"{val1} * {val2} = {MulTwoNums(val1,val2)}");
+            break;
+            case 4: System.Console.WriteLine($"{val1} / {val2} = {DivTwoNums(val1,val2)}");
+            break;
+            default: System.Console.WriteLine("Enter a valid Option");
+            break;
         }
     }
+    static void Main(string[] args) 
+    {
+    
+        bool repeat = true;
+        while (repeat)
+        {
+            ask();
+            System.Console.WriteLine("do you want to contine again (Y/N)?");
+            string continueProg = Console.ReadLine();
+            if (continueProg.ToUpper().Equals("N"))
+            {
+                repeat = false;
+            }
+            else
+            {
+                repeat = true;
+            }
+        }
+    }   
+    
 }
--- myproject2/obj/Debug/net10.0/myproject2.AssemblyInfo.cs
+++ myproject2/obj/Debug/net10.0/myproject2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myproject2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98aaff23bccdded08301f5cfc8d08275e90bd38e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6259e33b00258bc86d774d5d2876e92067825215")]
 [assembly: System.Reflection.AssemblyProductAttribute("myproject2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myproject2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
